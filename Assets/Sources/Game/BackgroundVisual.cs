d5acbbb48ea7446b6eed4390598897cab91e832d
Artur Salyakhov <artykod@gmail.com>
2015-06-24 22:04:57 +0500

    - утилитные скрипты - тестовый фон - структура папок

---
+++ Assets/Sources/Game/BackgroundVisual.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BackgroundVisual : MonoBehaviour {
+
+	private MeshRenderer meshRenderer = null;
+	private MovieTexture movie = null;
+
+	private void Awake() {
+		meshRenderer = GetComponent<MeshRenderer>();
+		movie = meshRenderer.sharedMaterial != null ? meshRenderer.sharedMaterial.mainTexture as MovieTexture : null;
+
+		movie.loop = true;
+		movie.Play();
+	}
+}
---
+++ Assets/Sources/GameCore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameCore : MonoBehaviour {
+
+	private static bool initialized = false;
+
+	[RuntimeInitializeOnLoadMethod]
+	private static void Initialize() {
+		if (!Application.isPlaying || initialized) {
+			return;
+		}
+
+		initialized = true;
+
+		DebugConsole.Initialize();
+		Config.Load();
+	}
+}
