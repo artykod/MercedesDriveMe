60b18d42a0d0978a3401a71578883bc06b8936f0
Artur Salyakhov <artykod@gmail.com>
2015-06-25 10:15:56 +0500

    правки физики машинки

--- Assets/Sources/Game/Car.cs
+++ Assets/Sources/Game/Car.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Car : MonoBehaviour {
 
@@ -17,20 +16,26 @@ public class Car : MonoBehaviour {
 
         Vector2 target = Camera.main.ScreenToWorldPoint(mouse);
 
+		Vector2 dir = target - new Vector2(transform.position.x, transform.position.y);
+		float distance = dir.magnitude;
+		Vector2 rotation = transform.rotation * Vector2.up;
+
 		if (Input.GetMouseButton(0)) {
-			Vector2 dir = target - new Vector2(transform.position.x, transform.position.y);
-			float distance = dir.magnitude;
-			Vector2 rotation = transform.rotation * Vector2.up;
 
-			Vector2 proj = rotation * Vector2.Dot(rotation, (dir / distance) * Mathf.Min(5f, distance * 4f));
+			float velocityMagnitude = Mathf.Min(8f, distance * 4f);
+			float dot = Vector2.Dot(rotation, (dir / distance) * velocityMagnitude);
+			Vector2 proj = rotation * dot;
 
-			body.velocity = proj * Time.deltaTime * 50f;
+			Vector2 velocity = Vector2.Lerp(body.velocity, proj * Time.deltaTime * 50f, 0.1f);
+			body.velocity = rotation * Vector2.Dot(rotation, velocity);
 
-			float k = Mathf.Min(distance, 1f);
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion2DHelper.RotationWithDirection(new Vector2(dir.y, -dir.x)), 0.25f * k * k);
+			float k = Mathf.Min(Mathf.Pow(distance, 2f), 1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion2DHelper.RotationWithDirection(new Vector2(dir.y, -dir.x)), 0.1f * k);
+		} else {
+			body.velocity = rotation * Vector2.Dot(rotation, body.velocity);
 		}
 
-		body.velocity *= 0.95f;
+		body.velocity *= 0.96f;
 		body.angularVelocity = 0f;
 	}
 }
